793a145ad47d5d12adb103f2484baab205bfe80b
penn <penn201500@gmail.com>
2024-04-23 15:14:12 +0800

    adjust popup display style to adapt dark-mode and light-mode

--- GoogleAdvSearchHint.js
+++ GoogleAdvSearchHint.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Google Search Bar Info Popup
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Show a popup around the Google search bar when hovering or clicking on it, and hide it when the cursor leaves.
 // @author       You
 // @match        https://www.google.com/*
@@ -10,6 +10,13 @@
 
 (function () {
   "use strict";
+
+  // Inject Google Fonts into the head of the document
+  const linkElement = document.createElement("link");
+  linkElement.href =
+    "https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@400;600&display=swap";
+  linkElement.rel = "stylesheet";
+  document.head.appendChild(linkElement);
 
   let hidePopupTimeout;
 
@@ -31,6 +38,16 @@
     if (existingPopup) {
       existingPopup.remove();
     }
+  }
+
+  let isDarkMode = false;
+  try {
+    isDarkMode =
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches;
+    console.log("Dark mode is " + (isDarkMode ? "enabled" : "disabled") + ".");
+  } catch (error) {
+    console.log("Failed to determine the color scheme mode.", error);
   }
 
   // Create a function to show the popup
@@ -56,72 +73,99 @@
     popup.style.whiteSpace = "nowrap"; // Prevents wrapping of content
     popup.style.overflow = "hidden"; // Keeps content from spilling out
     popup.style.textOverflow = "ellipsis"; // Adds ellipsis to overflowing text
+    // Apply the background color based on isDarkMode
+    popup.style.backgroundColor = isDarkMode
+      ? "rgba(50, 50, 50, 0.9)"
+      : "#f9f9f9";
+    popup.style.color = isDarkMode ? "#CCC" : "black"; // Lighter text color for better contrast in dark mode
+    popup.style.border = isDarkMode ? "1px solid #666" : "1px solid #ccc"; // Adjust border color for dark mode
 
     // Adding inner HTML for styled content with examples
     popup.innerHTML = `
-        <div class="title">Google Advanced Search Tips:</div>
-        <ul class="tips-list">
-            <li><code class="code">""</code><span class="colon">:</span><span class="description">Exact phrase search.</span><span class="example">e.g., "exact phrase"</span></li>
-            <li><code class="code">-</code><span class="colon">:</span><span class="description">Exclude words.</span><span class="example">e.g., -unwanted -words</span></li>
-            <li><code class="code">*</code><span class="colon">:</span><span class="description">Wildcard/fuzzy search.</span><span class="example">e.g., *wildcard*</span></li>
-            <li><code class="code">site</code><span class="colon">:</span><span class="description">Limit results to a specific site.</span><span class="example">e.g., site:example.com</span></li>
-            <li><code class="code">filetype</code><span class="colon">:</span><span class="description">Search for files like PDF, DOCX.</span><span class="example">e.g., filetype:pdf</span></li>
-            <li><code class="code">intitle</code><span class="colon">:</span><span class="description">Words must appear in the title.</span><span class="example">e.g., intitle:weather report</span></li>
-            <li><code class="code">inurl</code><span class="colon">:</span><span class="description">Words must appear in the URL.</span><span class="example">e.g., inurl:blog</span></li>
-            <li><code class="code">intext</code><span class="colon">:</span><span class="description">Words must appear in the text.</span><span class="example">e.g., intext:"privacy policy"</span></li>
-            <li><code class="code">OR</code><span class="colon">:</span><span class="description">Search for either word.</span><span class="example">e.g., vacation London OR Paris</span></li>
-            <li><code class="code">related</code><span class="colon">:</span><span class="description">Find related websites.</span><span class="example">e.g., related:example.com</span></li>
-            <li><code class="code">cache</code><span class="colon">:</span><span class="description">Show Google's cached version.</span><span class="example">e.g., cache:example.com</span></li>
-        </ul>
-        <style>
-            .title {
-                font-weight: bold;
-                margin-bottom: 5px;
-                color: gray;
-            }
-            .tips-list {
-                list-style-type: none;
-                padding: 0;
-                margin: 0;
-            }
-            .code {
-                font-weight: bold;
-                color: blue;
-            }
-            .colon {
-                font-weight: bold;
-                color: orange;
-            }
-            .description {
-                color: gray;
-                margin-left: 1%;
-            }
-            .example {
-                display: none;
-                color: black;
-                font-style: italic;
-                margin-left: 1%;
-            }
-            .tips-list li:hover .example {
-                display: inline;
-            }
-            #customPopup {
-                max-width: 400px; /* Set a fixed maximum width for the popup */
-                width: 100%; /* This makes the popup take full width up to its max-width */
-                box-sizing: border-box; /* This ensures padding and borders are included in the width */
-                overflow: hidden; /* Hide the overflow content */
-            }
-            #customPopup:hover {
-                max-height: none; /* Show all content on hover */
-            }
-            .closing-remark {
-                font-size: 10px;
-                margin-top: 5px;
-                color: darkgreen;
-            }
-        </style>
-        <div class="closing-remark">Hover over the tips to see examples or out to close this box.</div>
-        `;
+          <div class="title">Google Advanced Search Tips:</div>
+          <hr class="custom-hr" />
+          <ul class="tips-list">
+              <li><code class="code">""</code><span class="colon">:</span><span class="description">Exact phrase search.</span><span class="example">e.g., "exact phrase"</span></li>
+              <li><code class="code">-</code><span class="colon">:</span><span class="description">Exclude words.</span><span class="example">e.g., -unwanted -words</span></li>
+              <li><code class="code">*</code><span class="colon">:</span><span class="description">Wildcard/fuzzy search.</span><span class="example">e.g., *wildcard*</span></li>
+              <li><code class="code">site</code><span class="colon">:</span><span class="description">Limit results to a specific site.</span><span class="example">e.g., site:example.com</span></li>
+              <li><code class="code">filetype</code><span class="colon">:</span><span class="description">Search for files like PDF, DOCX.</span><span class="example">e.g., filetype:pdf</span></li>
+              <li><code class="code">intitle</code><span class="colon">:</span><span class="description">Words must appear in the title.</span><span class="example">e.g., intitle:weather report</span></li>
+              <li><code class="code">inurl</code><span class="colon">:</span><span class="description">Words must appear in the URL.</span><span class="example">e.g., inurl:blog</span></li>
+              <li><code class="code">intext</code><span class="colon">:</span><span class="description">Words must appear in the text.</span><span class="example">e.g., intext:"privacy policy"</span></li>
+              <li><code class="code">OR</code><span class="colon">:</span><span class="description">Search for either word.</span><span class="example">e.g., vacation London OR Paris</span></li>
+              <li><code class="code">related</code><span class="colon">:</span><span class="description">Find related websites.</span><span class="example">e.g., related:example.com</span></li>
+              <li><code class="code">cache</code><span class="colon">:</span><span class="description">Show Google's cached version.</span><span class="example">e.g., cache:example.com</span></li>
+          </ul>
+          <hr class="custom-hr" />
+          <style>
+                          .title, .description, .example, .code, .closing-remark {
+                      font-family: 'Source Code Pro', monospace;
+                  }
+                          @keyframes fadeIn {
+              from { opacity: 0; }
+              to { opacity: 1; }
+          }
+          .custom-hr {
+              border: 0;
+              height: 1px;
+              background-image: linear-gradient(to right, transparent, ${
+                isDarkMode ? "#fff" : "#555"
+              }, transparent);
+              margin: 10px 0;
+              opacity: 0;
+              animation: fadeIn 1s ease-in-out forwards; /* Apply the fadeIn animation */
+          }
+              .title {
+                  font-weight: bold;
+                  margin-bottom: 5px;
+                  color: ${isDarkMode ? "white" : "gray"};
+              }
+              .tips-list {
+                  list-style-type: none;
+                  padding: 0;
+                  margin: 0;
+              }
+              .code {
+                  font-weight: bold;
+                    color: ${isDarkMode ? "lightblue" : "blue"};
+              }
+              .colon {
+                  font-weight: bold;
+                  color: orange;
+              }
+              .description {
+                    color: ${isDarkMode ? "white" : "gray"};
+                  margin-left: 1%;
+              }
+              .example {
+                  display: none;
+                  font-style: italic;
+                  margin-left: 1%;
+                  color: ${
+                    isDarkMode ? "#BBB" : "#555"
+                  }; // Lighter for dark mode but still legible
+              }
+              .tips-list li:hover .example {
+                  display: inline;
+              }
+              #customPopup {
+                  max-width: 400px; /* Set a fixed maximum width for the popup */
+                  width: 100%; /* This makes the popup take full width up to its max-width */
+                  box-sizing: border-box; /* This ensures padding and borders are included in the width */
+                  overflow: hidden; /* Hide the overflow content */
+              }
+              #customPopup:hover {
+                  max-height: none; /* Show all content on hover */
+              }
+              .closing-remark {
+                  font-size: 10px;
+                  margin-top: 5px;
+                  color: ${isDarkMode ? "lightgreen" : "darkgreen"};
+              }
+          </style>
+          <div class="closing-remark">Hover over the tips to see examples or out to close this box.</div>
+          `;
 
     // Append the popup to the body of the page
     document.body.appendChild(popup);
